8425b7df54a86a62c9da79ea46a5bb26005efada
Agu <agustin.verdugo@ug.uchile.cl>
2025-10-20 20:41:51 -0300

    fix: app didn't compile

--- frontend/src/components/UserPage.tsx
+++ frontend/src/components/UserPage.tsx
@@ -1,8 +1,7 @@
 import type { UserData } from "../types/user";
 import userService from "../services/user"
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 //falta css
 const UserPage = () => {
